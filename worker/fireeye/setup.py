91bc39a38d9b82527794a5a1d1daefd9a44dee90
Taskr <adam.p.trask@gmail.com>
2016-06-18 00:19:03 -0400

    Updating FireEye plugin

--- worker/fireeye/setup.py
+++ worker/fireeye/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="fireeye",
     version="0.1.4",
-    author="Marcus LaFerrera (@mlaferrera) & Aaron Gee-Clough (@gclef_)",
+    author="Marcus LaFerrera (@mlaferrera), Aaron Gee-Clough (@gclef_) & Adam Trask (@taskr)",
     url="https://github.com/PUNCH-Cyber/stoq-plugins-public",
     license="Apache License 2.0",
     description="Submit a file to a Fireeye MAS/AX via filesystem or API",
